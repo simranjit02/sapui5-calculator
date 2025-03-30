40bf66689515ab2b184adb3f458fc37aac7411c8
Simranjit Singh <simranjitsingh58648@gmail.com>
2025-03-30 09:01:16 +0000

    add functionality in controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,6 +5,26 @@ sap.ui.define([
 
     return Controller.extend("project1.controller.View1", {
         onInit() {
+        },
+        onAdd: function(){
+            var numOne= +this.getView().byId("firstNum").getValue();
+            var numTwo= +this.getView().byId("secondNum").getValue();
+            this.getView().byId("final-result").setValue(numOne + numTwo);
+        },
+        onMinus: function(){
+            var numOne= +this.getView().byId("firstNum").getValue();
+            var numTwo= +this.getView().byId("secondNum").getValue();
+            this.getView().byId("final-result").setValue(numOne - numTwo);
+        },
+        onMultiply: function(){
+            var numOne= +this.getView().byId("firstNum").getValue();
+            var numTwo= +this.getView().byId("secondNum").getValue();
+            this.getView().byId("final-result").setValue(numOne * numTwo);
+        },
+        onDivide: function(){
+            var numOne= +this.getView().byId("firstNum").getValue();
+            var numTwo= +this.getView().byId("secondNum").getValue();
+            this.getView().byId("final-result").setValue(numOne / numTwo);
         }
     });
 });
